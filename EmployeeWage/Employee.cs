6297c86a5f39c08fc4714ea03bda3066391ae037
mendhe1020 <anurag.mendhe14@gmail.com>
2022-09-10 19:36:35 +0530

    [Anurag] Add: Calculate Wage

--- EmployeeWage/Employee.cs
+++ EmployeeWage/Employee.cs
@@ -11,16 +11,22 @@ namespace EmployeeWage
         public int Empis;
         public void EmpWage()
         {
+            int WagePrHr = 20;
+            int FullDayHr = 8;
+
             Random random = new Random();
             int Empis = random.Next(2);
+
             if (Empis == 0)
             {
                 Console.WriteLine("Emploee is Present");
+                Console.WriteLine(WagePrHr * FullDayHr);
             }
             else
             {
                 Console.WriteLine("Emploee is Abcent");
             }
+
         }
     }
 }
